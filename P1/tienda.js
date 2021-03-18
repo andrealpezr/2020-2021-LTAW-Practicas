8d93021b83f3df1f92d0c3852284c665b8183585
andrealpezr <andrealpezrecio@gmail.com>
2021-03-18 17:05:00 +0100

    Importo todos los modulos

---
+++ P1/tienda.js
@@ -0,0 +1,29 @@
+//-- Importo los modulos http, fs y url
+const http = require('http');
+const fs = require('fs');
+//--const URL = require('url').URL;
+
+//-- Definir el puerto a utilizar
+const PUERTO = 9000;
+
+//-- Crear el servidor
+const server = http.createServer((req, res) => {
+    
+  //-- Indicamos que se ha recibido una petición
+  console.log("Petición recibida!");
+
+  //-- Cabecera que indica el tipo de datos del
+  //-- cuerpo de la respuesta: Texto plano
+  res.setHeader('Content-Type', 'text/plain');
+
+  //-- Mensaje del cuerpo
+  res.write("Soy el Happy server!!\n");
+
+  //-- Terminar la respuesta y enviarla
+  res.end();
+});
+
+//-- Activar el servidor: ¡Que empiece la fiesta!
+server.listen(PUERTO);
+
+console.log("Server 5: Happy server activado!. Escuchando en puerto: " + PUERTO);
