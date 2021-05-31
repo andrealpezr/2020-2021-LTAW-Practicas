bc7899a155e2267bd4d154709dc2b87ed35b60b9
andrealpezr <andrealpezrecio@gmail.com>
2021-05-31 17:52:13 +0200

    p4

--- P4/Chat/index.js
+++ P4/Chat/index.js
@@ -1,8 +1,10 @@
 const electron = require('electron');
+const ip_address = require('ip');
+const QRCode = require('qrcode');
 
 console.log("Hola desde el proceso de la web...");
 
-var PUERTO = 8080;
+var PUERTO = 8080
 
 //-- Obtener elementos de la interfaz
 const btn_test = document.getElementById("btn_test");
@@ -12,6 +14,10 @@ const info2 = document.getElementById("info2");
 const info3 = document.getElementById("info3");
 const info4 = document.getElementById("info4");
 const info5 = document.getElementById("info5");
+const info6 = document.getElementById("info6");
+const info7 = document.getElementById("info7");
+const URL = document.getElementById("URL");
+const QR = document.getElementById("QR");
 const print = document.getElementById("print");
 const ip_address = document.getElementById("ip_address");
 
@@ -20,9 +26,11 @@ const ip_address = document.getElementById("ip_address");
 //-- el proceso princpal
 info1.textContent = process.arch;
 info2.textContent = process.platform;
-info3.textContent = process.node;
-info4.textContent = process.chrome;
-info5.textContent = process.electron;
+info3.textContent = process.cwd();
+info4.textContent = process.versions.node;
+info5.textContent = process.versions.chrome;
+info6.textContent = process.versions.electron;
+
 ip_address.textContent = ip.address();
 info3.textContent += ":" + PUERTO;
 
@@ -39,6 +47,14 @@ btn_test.onclick = () => {
 electron.ipcRenderer.on('print', (event, message) => {
     console.log("Recibido: " + message);
     print.textContent = message;
-  });
+});
+
+//-- Mensaje recibido del proceso MAIN
+electron.ipcRenderer.on('chat', (event, msg) => {
+    console.log("Recibido: " + msg);
+    display.innerHTML += '<p style="color:black">' + msg.username + " :" 
+                         + msg.message + '</p>';
+});
+
 
 
